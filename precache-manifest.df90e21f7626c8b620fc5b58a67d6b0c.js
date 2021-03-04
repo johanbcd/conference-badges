a28f85ad80b42ffab6fec02297eca77d3968cf8f
Johan Baracaldo <johanbcd@gmail.com>
2021-03-04 19:09:44 +0100

    Updates

---
+++ precache-manifest.df90e21f7626c8b620fc5b58a67d6b0c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "092861194193f39c5853",
+    "url": "/conference-badges​/static/css/main.d6cfa801.chunk.css"
+  },
+  {
+    "revision": "092861194193f39c5853",
+    "url": "/conference-badges​/static/js/main.09286119.chunk.js"
+  },
+  {
+    "revision": "97e66cc78f97bba9a692",
+    "url": "/conference-badges​/static/js/runtime~main.97e66cc7.js"
+  },
+  {
+    "revision": "46b60d3f30cd750162e1",
+    "url": "/conference-badges​/static/css/2.7058ddee.chunk.css"
+  },
+  {
+    "revision": "46b60d3f30cd750162e1",
+    "url": "/conference-badges​/static/js/2.46b60d3f.chunk.js"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/conference-badges​/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/conference-badges​/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/conference-badges​/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "4fb20dfbe3ad8fa6d0521644f50cf055",
+    "url": "/conference-badges​/static/media/500Error.4fb20dfb.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/conference-badges​/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "9623db08b42d4467e25ac1465e78236f",
+    "url": "/conference-badges​/static/media/404.9623db08.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/conference-badges​/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "5c23880d6c21f45e5e8581fde45a89a1",
+    "url": "/conference-badges​/static/media/LogoBlue.5c23880d.svg"
+  },
+  {
+    "revision": "9b062cc1d4c2ba37208bf0fdf15500dc",
+    "url": "/conference-badges​/index.html"
+  }
+];
