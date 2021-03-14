c90c55ded5072a40c30f3994d36d662df56a09dd
Johan Baracaldo <johanbcd@gmail.com>
2021-03-14 22:48:31 +0100

    Updates

---
+++ precache-manifest.edef814a89a3ed04f7f8f82a455e546c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b412f9ff0aae9c7618b",
+    "url": "/static/css/main.f983ac31.chunk.css"
+  },
+  {
+    "revision": "7b412f9ff0aae9c7618b",
+    "url": "/static/js/main.7b412f9f.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5bb2e3cb5249db65f877",
+    "url": "/static/css/2.7058ddee.chunk.css"
+  },
+  {
+    "revision": "5bb2e3cb5249db65f877",
+    "url": "/static/js/2.5bb2e3cb.chunk.js"
+  },
+  {
+    "revision": "26c371d7e34bdbd233a7f9cdd2eeedae",
+    "url": "/static/media/tech.26c371d7.svg"
+  },
+  {
+    "revision": "1554a60384e74e2710f7ad6819d644cb",
+    "url": "/static/media/icon.1554a603.svg"
+  },
+  {
+    "revision": "b139830b32a5e54651c86b100e5c5a70",
+    "url": "/static/media/web-devices.b139830b.svg"
+  },
+  {
+    "revision": "4fb20dfbe3ad8fa6d0521644f50cf055",
+    "url": "/static/media/500Error.4fb20dfb.svg"
+  },
+  {
+    "revision": "9623db08b42d4467e25ac1465e78236f",
+    "url": "/static/media/404.9623db08.svg"
+  },
+  {
+    "revision": "2192430928c3fede3e2a0411cd74d20e",
+    "url": "/static/media/circuit.21924309.svg"
+  },
+  {
+    "revision": "5c23880d6c21f45e5e8581fde45a89a1",
+    "url": "/static/media/LogoBlue.5c23880d.svg"
+  },
+  {
+    "revision": "4b47c3a95edc9fc44cf80e2bf92072d3",
+    "url": "/index.html"
+  }
+];
